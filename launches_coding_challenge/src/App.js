ec977a68be5069523fa85bf30c24fe871ce122d0
Andrew Frazier <acorbettfrazier@gmail.com>
2022-01-31 12:45:49 -0500

    list of launches and failures

--- launches_coding_challenge/src/App.js
+++ launches_coding_challenge/src/App.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
+
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    Axios.get(
+      'https://api.spacexdata.com/v4/launches'
+    )
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log("there was an error", err));
+  };
+
+  useEffect(() => {
+    getData();
+    console.log(data)
+  });
+
   return (
     <div className="App">
-      Launch
+      <h1>Launches</h1>
+      <div style={{ height: '100vh', width: '100vw' }}>{data.map((data, index) => {
+        return (
+          <li key={index}>{data.failures.length}</li>
+        );
+      })}</div>
     </div>
   );
 }
