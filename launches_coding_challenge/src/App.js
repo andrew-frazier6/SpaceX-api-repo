960c57cf9330ad0a429e7571f8aa3da67379f5ec
Andrew Frazier <acorbettfrazier@gmail.com>
2022-01-31 13:50:22 -0500

    pie data split between success and failure simple

--- launches_coding_challenge/src/App.js
+++ launches_coding_challenge/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Pie } from '@visx/shape';
+import { Group } from '@visx/group';
+import { Text } from '@visx/text';
 import Axios from "axios";
 
 import './App.css';
@@ -6,6 +9,11 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
+  const pieData = [{
+    "success": 147
+  }, {
+    "failure": 5
+  }];
 
   const getData = () => {
     Axios.get(
@@ -20,11 +28,26 @@ function App() {
   useEffect(() => {
     getData();
     console.log(data)
-  });
+  }, []);
+
+  const width = 300;
+  const half = width / 2;
 
   return (
     <div className="App">
       <h1>Launches</h1>
+      <svg className='pie-chart' >
+        <Group top={half} left={half}>
+          <Pie
+            data={pieData}
+            // pieValue={(data) => (data.failure / data.success) * 100}
+            pieValue={pieData.length}
+            outerRadius={half}
+            innerRadius={half - 8}
+            padAngle={0.1}>
+          </Pie>
+        </Group>
+      </svg>
       <div style={{ height: '100vh', width: '100vw' }}>{data.map((data, index) => {
         return (
           <li key={index}>{data.failures.length}</li>
