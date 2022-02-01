15bf76e2d8fa4aae0142c3b7445d7051197e405b
Andrew Frazier <acorbettfrazier@gmail.com>
2022-02-01 14:04:51 -0500

    separated page content

--- launches_coding_challenge/src/App.js
+++ launches_coding_challenge/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Pie } from '@visx/shape';
 import { Group } from '@visx/group';
-// import { Text } from '@visx/text';
+import { Text } from '@visx/text';
 import Axios from "axios";
 import './App.css';
 
@@ -16,6 +16,7 @@ function App() {
     )
       .then((res) => {
         setData(res.data);
+        console.log(data)
         setModeledData(
           [{
             result: "Success",
@@ -36,46 +37,62 @@ function App() {
     getData();
   }, []);
 
-  const width = 300;
+  const width = 600;
   const half = width / 2;
 
   return (
     <div className="App">
-      <h1>Launches</h1>
-      <svg className='pie-chart' >
-        <Group top={half} left={half}>
-          <Pie
-            data={modeledData}
-            pieValue={((data) => {
-              const failures = data?.result === 'Failure' ? data.total : 0;
-              const success = data?.result === 'Success' ? data.total : 0;
-              return success + failures;
-            })}
-            outerRadius={half}
-            innerRadius={({ data }) => {
-              console.log(active)
-              const size = active === data.result ? 14 : 8;
-              return half - size;
-            }}
-            padAngle={0.007}>
-            {(pie) => {
-              return pie.arcs.map((arc) => {
-                return (
-                  <g key={arc.data.result} style={{ cursor: 'pointer' }} onMouseEnter={() => setActive(arc.data.result)}
-                    onMouseLeave={() => setActive(null)}>
-                    <path d={pie.path(arc)} fill={arc.data.color}></path>
-                  </g>
-                )
-              })
-            }}
-          </Pie>
-        </Group>
-      </svg>
-      <div style={{ height: '100vh', width: '100vw' }}>{data.map((data, index) => {
-        return (
-          <li key={index}>{data.name}</li>
-        );
-      })}</div>
+      {/* HOME */}
+      <div className='page-container'>
+        <div className='sky'>
+          <div className='stars'>
+            <h1 style={{ color: 'white', textDecoration: 'underline', margin: '2.5%' }}>Space X Launches</h1>
+            <ul className='launches-list'>{data.map((data, index) => {
+              return (
+                <ul className="launches-item" key={index}>{data.name} - {data.date_utc.slice(0, 10)}</ul>
+              );
+            })}</ul>
+          </div>
+        </div>
+      </div>
+      <div className='page-container2'>
+        <div className='sky'>
+          <div className='stars'>
+            <div className='pie-chart-container'>
+              <svg width={width} height={width} >
+                <Group top={half} left={half}>
+                  <Pie
+                    data={modeledData}
+                    pieValue={((data) => {
+                      const failures = data?.result === 'Failure' ? data.total : 0;
+                      const success = data?.result === 'Success' ? data.total : 0;
+                      return success + failures;
+                    })}
+                    outerRadius={half}
+                    innerRadius={({ data }) => {
+                      // console.log(active)
+                      const size = active === data.result ? 20 : 12;
+                      return half - size;
+                    }}
+                    padAngle={0.01}>
+                    {(pie) => {
+                      return pie.arcs.map((arc) => {
+                        return (
+                          <g key={arc.data.result} style={{ cursor: 'pointer' }} onMouseEnter={() => setActive(arc.data.result)}
+                            onMouseLeave={() => setActive(null)}>
+                            <path d={pie.path(arc)} fill={arc.data.color}></path>
+                          </g>
+                        )
+                      })
+                    }}
+                  </Pie>
+                  <Text textAnchor='middle' >HELLO</Text>
+                </Group>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
